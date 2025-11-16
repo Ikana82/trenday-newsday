501e489756ade245f9d5dba97b66157109471518
ikana82 <ikanurasih82@gmail.com>
2025-11-16 23:14:31 +0700

    Finish 2

--- news-app/src/components/navbar.tsx
+++ news-app/src/components/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <nav className=" shadow-sm dark:text-white text-black">
+    <nav className="bg-white dark:bg-black shadow-sm dark:text-white text-black">
       <div className="container mx-auto px-4 py-4 flex flex-col gap-4">
         {/* Top 1 */}
         <div className="flex flex-row items-center justify-between w-full gap-4">
